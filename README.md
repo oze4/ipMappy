5a133f45594f3dcc55e37f8e92d7d3283229e27e
Matt Oestreich <oze41998@yahoo.com>
2019-01-20 21:29:07 -0600

    Major refactor, added Jupyter Notebook functionality

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ The core API module relies on https://ipstack.com - so you will need an API key 
 ###### Example:
 
 ```python
+
 from ipMappy.ApiContext import IpStack
 
 my_api_key = "abcdefg"
@@ -14,21 +15,15 @@ my_api_key = "abcdefg"
 # main ipMappy object
 ipstack = IpStack(my_api_key)
 
-# Grab public info about IP or host
-# The IpStack API will resolve hostnames for us
+# Grab public info about IP or host - the IpStack API will resolve host names for us
 google_info = ipstack.get_ip_info('google.com')
-print(google_info['ip'])
-print(google_info['latitude'])
-print(google_info['longitude'])
-# The above will tie IP info to the 'google_info' variable in JSON string format
-# The above command also de-serializes the JSON into our object
-# Therefore, this is also valid
-print(ipstack.ip)
-print(ipstack.latitude)
-print(ipstack.longitude)
+print(google_info.ip)
+print(google_info.latitude)
+print(google_info.longitude)
 
 # Grab public IP info about the network which the request was sent from
 current_ip_info = ipstack.get_current_ip_info()
-# Same as above, this method returns a JSON string as well as de-serializing the JSON into our instance attributes
+print(current_ip_info)
+
 ```
 
--- ipMappy/api.py
+++ ipMappy/api.py
@@ -1,14 +1,18 @@
 """
 MIT License
+
 Copyright (c) 2019 Matthew Oestreich
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,11 +22,28 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+import requests
+from ipMappy.ip_info import MappyJSON
 
-from ipMappy.api import IpStack
 
+class IpStack:
+    """Api client for https://ipstack.com
+    Methods that get API data always return JSON"""
 
-# instantiate object
-ips = IpStack("83e165094b4d4f05f6aa904c4b6484db")
-current_ip_info = ips.get_current_ip_info()
-print(type(current_ip_info))
+    __base_url = "http://api.ipstack.com"
+
+    def __init__(self, api_key):
+        if not isinstance(api_key, str):
+            raise TypeError('api_key is not a string!')
+        self.ApiKey = api_key
+
+    def get_current_ip_info(self):
+        """:return: MappyJSON object (JSON as dict)"""
+        res = requests.get(f'{self.__base_url}/check?access_key={self.ApiKey}')
+        return MappyJSON(res.json())
+
+    def get_ip_info(self, ip_or_hostname):
+        """:param ip_or_hostname: IP address or hostname of target host - the IpStack API resolves the hostname for us
+        :return: MappyJSON object (JSON as dict)"""
+        res = requests.get(f'{self.__base_url}/{ip_or_hostname}?access_key={self.ApiKey}')
+        return MappyJSON(res.json())
